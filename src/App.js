80c2d186125b52e7b0c4c31b6609a7d464bcca93
Alipova <alipova09022003@gmail.com>
2023-11-21 21:26:57 +0600

    backend link

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 import './App.css';
 
 const api = axios.create({
-  baseURL: 'http://127.0.0.1:8000',
+  baseURL: 'https://fastapi-7dub.onrender.com',
 });
 
 function App() {
